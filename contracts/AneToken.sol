273945425e6b5961ef4500b015b7fab341277135
deusexmachina892 <deusexmachina892@gmail.com>
2018-11-25 08:24:35 -0500

    3 Delegated Transfer

--- contracts/AneToken.sol
+++ contracts/AneToken.sol
@@ -1,16 +1,25 @@
 pragma solidity ^0.4.2;
 
 contract AneToken{
+    //ERC 20 requires a name and a symbol
     string public constant name = 'Ane Token';
     string public constant symbol = 'ANE';
     string public constant standard = 'Ane Token v1.0'; 
     uint256 public totalSupply;
 
     mapping(address => uint256) public balanceOf;
+    //allowance
+    mapping(address => mapping(address=>uint256)) public allowance;
 
     event Transfer(
         address indexed _from,
         address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
         uint256 _value
     );
 
@@ -19,8 +28,8 @@ contract AneToken{
         totalSupply = _initialSupply;
 
     }
-    //Transfer
 
+    //Transfer function is required
     function transfer(address _to, uint256 _value) public returns(bool success){
     //Exception if does not have enough
     require(balanceOf[msg.sender] >= _value);
@@ -32,6 +41,40 @@ contract AneToken{
     //Transfer event
     Transfer(msg.sender, _to, _value);
 
+    //bool return to indicate transaction status
     return true;
+    }
+
+    //Delegated Transfer
+
+    //approve
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        //Allowance
+        allowance[msg.sender][_spender] = _value; 
+        //Approve Event
+        Approval(msg.sender, _spender, _value);
+        return true;     
+    }
+
+    //transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+
+        //Require _from has enough tokens
+        require(balanceOf[_from] >= _value);
+        //Require allowance is big enough
+        require(allowance[_from][msg.sender] >= _value);
+
+        //Transfer Event
+        Transfer(_from, _to, _value); 
+
+        //Change balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        //Update Allowance
+        allowance[_from][msg.sender] -= _value;
+        
+        //return a boolean
+        return true;    
     }
 }
--- test/AneToken.js
+++ test/AneToken.js
@@ -43,10 +43,10 @@ contract('AneToken', function(accounts){
             assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
             return tokenInstance.transfer.call(accounts[1], 250000,{from: accounts[0]});
         }).then(function(success){
-            assert.equal(success, true, 'it returns');
+            assert.equal(success, true, 'it returns true');
             return tokenInstance.transfer(accounts[1], 250000,{from: accounts[0]});
         }).then(function(receipt){
-            console.log(receipt.logs);
+            //console.log(receipt.logs);
             assert.equal(receipt.logs.length, 1, 'triggers one event');
             assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
             assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
@@ -58,6 +58,68 @@ contract('AneToken', function(accounts){
             return tokenInstance.balanceOf(accounts[0]);
         }).then(function(balance){
             assert.equal(balance, 750000, 'it deducts the amount from the transferer')
-        })
+        });
+    });
+
+    it('approves tokens for delegated transfer', function(){
+        return AneToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function(success){
+            assert.equal(success, true, 'it returns true');
+            return tokenInstance.approve(accounts[1], 100, {from: accounts[0]});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokems are authorized by');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs the approved amount');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance){
+            assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+        });
+    });
+
+    it('handles delegated token transfer', function(){
+        return AneToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+            //Transfer some tokens to fromAccount
+            return tokenInstance.transfer(fromAccount, 100, {from: accounts[0]});
+        }).then(function(receipt){
+            //Approve spending account to spend 10 tokens from fromAccount
+            return tokenInstance.approve(spendingAccount, 10, {from: fromAccount})
+        }).then(function(receipt){
+            //Try something larger than the sender's balance
+            return tokenInstance.transferFrom(fromAccount, toAccount, 200,{from:spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            assert.equal(error.message.indexOf('revert') >= 0, true, 'cannot transfer balance larger than sender balance');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 20,{from:spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            assert.equal(error.message.indexOf('revert') >= 0, true, 'cannot transfer amount larger than allowance');
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 5, {from: spendingAccount});
+        }).then(function(success){
+            assert.equal(success, true, 'it returns true');
+            return tokenInstance.transferFrom(fromAccount, toAccount, 5, {from: spendingAccount});
+        }).then(function(receipt){
+            //console.log(receipt)
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Approval" event');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokems are transferred from');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
+            assert.equal(receipt.logs[0].args._value, 5, 'logs the transferred amount');
+            return tokenInstance.balanceOf(fromAccount);
+        }).then(function(balance){
+            assert.equal(balance, 95, 'deducts the amount from the sendingAccount');
+            return tokenInstance.balanceOf(toAccount);
+        }).then(function(balance){
+            assert.equal(balance, 5, 'adds the amount to the toAccount');
+            return tokenInstance.allowance(fromAccount, spendingAccount);
+        }).then(function(allowance){
+           // console.log(tokenInstance.contract.allowance)
+            assert.equal(allowance.toNumber(), 5, 'updates the allowance');
+        });
     })
 });
